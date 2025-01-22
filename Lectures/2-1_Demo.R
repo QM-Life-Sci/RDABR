594f6f0fe821ac13e0c4c4c29e41af4b769bee14
Kevin Middleton <middletonk@missouri.edu>
2025-01-21 22:22:13 -0600

    Add earth demo

---
+++ Lectures/2-1_Demo.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(readxl)
+
+theme_set(cowplot::theme_cowplot())
+
+LW <- read_excel("../data/Earth_Land_Water.xlsx") |> 
+  mutate(C_Land = cumsum(Land),
+         C_Water = cumsum(Water),
+         `Percent Water` = C_Water / (C_Water + C_Land),
+         Sample = 1:n())
+
+LW
+
+ggplot(LW, aes(x = Sample, y = `Percent Water`)) +
+  geom_line(color = "firebrick4", size = 2) +
+  scale_y_continuous(limits = c(0, 1))
+
+################################################################
+
+n_samples <- 10
+sample_size <- 10
+
+LW <- tibble(Sample = 1:n_samples,
+             Water = rbinom(n = n_samples, size = sample_size, prob = 0.86),
+             Land = sample_size - Water,
+             C_Land = cumsum(Land),
+             C_Water = cumsum(Water),
+             `Percent Water` = C_Water / (C_Water + C_Land))
+
+ggplot(LW, aes(x = Sample, y = `Percent Water`)) +
+  geom_line(color = "firebrick4", size = 2) +
+  scale_y_continuous(limits = c(0, 1))
+
