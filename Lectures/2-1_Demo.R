e19e16b4ffb63865e32c81c42c9144a2c23d8625
Kevin Middleton <middletonk@missouri.edu>
2025-01-23 20:19:18 -0600

    Add images to 2-1

--- Lectures/2-1_Demo.R
+++ Lectures/2-1_Demo.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
+library(DescTools)
 
 theme_set(cowplot::theme_cowplot())
 
@@ -12,7 +13,7 @@ LW <- read_excel("../data/Earth_Land_Water.xlsx") |>
 LW
 
 ggplot(LW, aes(x = Sample, y = `Percent Water`)) +
-  geom_line(color = "firebrick4", size = 2) +
+  geom_line(color = "firebrick4", linewidth = 2) +
   scale_y_continuous(limits = c(0, 1))
 
 ################################################################
@@ -21,13 +22,25 @@ n_samples <- 10
 sample_size <- 10
 
 LW <- tibble(Sample = 1:n_samples,
-             Water = rbinom(n = n_samples, size = sample_size, prob = 0.86),
+             Water = rbinom(n = n_samples, size = sample_size, prob = 0.71),
              Land = sample_size - Water,
              C_Land = cumsum(Land),
              C_Water = cumsum(Water),
              `Percent Water` = C_Water / (C_Water + C_Land))
 
+
+message("Samples: ", max(LW$C_Land) + max(max(LW$C_Water)))
+
 ggplot(LW, aes(x = Sample, y = `Percent Water`)) +
-  geom_line(color = "firebrick4", size = 2) +
+  geom_line(color = "firebrick4", linewidth = 2) +
   scale_y_continuous(limits = c(0, 1))
 
+################################################################
+
+BinomCI(9, 10,
+        conf.level = 0.95,
+        method = "clopper-pearson")
+
+BinomCI(81, 100,
+        conf.level = 0.95,
+        method = "clopper-pearson")
