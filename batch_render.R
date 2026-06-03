18e9618a1ade14d592b26aecf5cbe61d841e00c5
Kevin Middleton <middletonk@missouri.edu>
2026-06-03 14:57:37 -0500

    Update batch_render.R

--- batch_render.R
+++ batch_render.R
@@ -2,6 +2,58 @@
 library(quarto)
 options(warn=2)
 
+## Install packages
+pkgs <- c(
+  "AICcmodavg",
+  "broom",
+  "colorspace",
+  "cowplot",
+  "cvTools",
+  "DescTools",
+  "effectsize",
+  "estimatr",
+  "factoextra",
+  "GGally",
+  "ggExtra",
+  "ggforce",
+  "ggpubr",
+  "ggrepel",
+  "ggsci",
+  "ggstatsplot",
+  "ggtext",
+  "glue",
+  "gt",
+  "ICC",
+  "knitr",
+  "latex2exp",
+  "magick",
+  "modelsummary",
+  "multcomp",
+  "mvtnorm",
+  "paletteer",
+  "palmerpenguins",
+  "patchwork",
+  "performance",
+  "plotly",
+  "purrr",
+  "pwr",
+  "quarto",
+  "readxl",
+  "report",
+  "Rmpfr",
+  "see",
+  "smatr",
+  "swirl",
+  "tidyplots",
+  "tidyverse",
+  "viridis",
+  "wesanderson"
+)
+
+# Uncomment to install all packages:
+# pak::pak(pkgs)
+
+
 qmds <- list.files(pattern = "^[0-9]*-[1-6].*qmd$",
                    path = "Lectures/",
                    full.names = TRUE)
